c94a9c2eb729b0ab34f72c41ee3878635adadead
Marc S. Brooks <marc@mbrooks.info>
2023-03-27 13:37:36 -0700

    Synced L³ extension changes (0.6.0)

--- .devcontainer/ecosystem.config.js
+++ .devcontainer/ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps: [{
-    script: '.devcontainer/server.js',
+    script: 'restfulApiHandler/node_modules/lambda-edge-server/server.js --handler restfulApiHandler/src/app.js',
     error_file : '.devcontainer/error.log',
     out_file : '.devcontainer/debug.log',
     watch: ['restfulApiHandler/src'],
