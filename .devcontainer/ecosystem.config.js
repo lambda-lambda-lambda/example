7fdab989e1adeb2100a47fbbee347cb923baecae
Marc S. Brooks <marc@mbrooks.info>
2023-10-04 12:49:56 -0700

    Synced L³ CLI changes (0.4.0)

--- .devcontainer/ecosystem.config.js
+++ .devcontainer/ecosystem.config.js
@@ -3,9 +3,9 @@ module.exports = {
     script: 'node_modules/lambda-edge-server/server.js',
     args: '--handler src/app.js',
     cwd: 'restfulApiHandler',
-    error_file: '.devcontainer/error.log',
-    out_file: '.devcontainer/debug.log',
-    watch: ['restfulApiHandler/src'],
+    error_file: '../.devcontainer/error.log',
+    out_file: '../.devcontainer/debug.log',
+    watch: ['src'],
     watch_delay: 1000,
     instances: 1,
     exec_mode: 'fork'
